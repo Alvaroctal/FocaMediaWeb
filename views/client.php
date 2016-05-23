511c5a2cc63f595e181a9d9465d1a0dd748772e6
LuisIniguez <luis.iniguez.1991@gmail.com>
2016-05-23 10:26:49 +0200

    client view

---
+++ views/client.php
@@ -0,0 +1,95 @@
+
+  <div id="front-container" class="no-overflow">
+      <div class="grid">
+          <div class="row cells12">
+              <div class="cell colspan5 cell-explain">
+                  <h3>Start</h3><hr />
+                 <p>Connect with your local server by typing hist local ip and port and FocaMedia will remember it for you.</p>
+              </div>
+              <div class="cell colspan7">
+                  <div class="image-container">
+                      <div class="frame"><img src="https://cloud.githubusercontent.com/assets/4562922/14845442/8464d2ce-0c5e-11e6-9ae2-e0bc2b41009b.png"></div>
+                      <div class="image-overlay">
+                          <h2>Connect</h2>
+                          <p>
+                              This is displayed once you start the aplication.
+                          </p>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div class="row cells12">
+              <div class="cell colspan7">
+                  <div class="image-container">
+                      <div class="frame"><img src="https://cloud.githubusercontent.com/assets/4562922/14846608/f1558170-0c64-11e6-9c57-be9a6c16be0a.png"></div>
+                      <div class="image-overlay">
+                          <h2>Browse your media</h2>
+                          <p>
+                              Easy, beautiful and intuitive
+                          </p>
+                      </div>
+                  </div>
+              </div>
+              <div class="cell colspan5 cell-explain">
+                  <h3>Browse your media</h3><hr />
+                  <p>You can browse your movies and series with a short info about it.</p>
+              </div>
+          </div>
+          <div class="row cells12">
+              <div class="cell colspan5 cell-explain">
+                  <h3>Undecided?</h3><hr />
+                 <p>Thousands of movies and have no clue about what to watch?.</p>
+                <p>No problem. Focamedia serves you with trailers and information about the movie or serie you want to watch</p>
+              </div>
+              <div class="cell colspan7">
+                  <div class="image-container">
+                      <div class="frame"><img src="https://cloud.githubusercontent.com/assets/4562922/14845442/8464d2ce-0c5e-11e6-9ae2-e0bc2b41009b.png"></div>
+                      <div class="image-overlay">
+                          <h2>Info, trailers and more</h2>
+                          <p>
+                              What you watch every time you select a media.
+                          </p>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div class="row cells12">
+              <div class="cell colspan7">
+                  <div class="image-container">
+                      <div class="frame"><img src="https://cloud.githubusercontent.com/assets/4562922/14846608/f1558170-0c64-11e6-9c57-be9a6c16be0a.png"></div>
+                      <div class="image-overlay">
+                          <h2>Looking for something?</h2>
+                          <p>You type it, you get it.</p>
+                      </div>
+                  </div>
+              </div>
+              <div class="cell colspan5 cell-explain">
+                  <h3>Too much media?</h3><hr />
+                  <p>
+                      FocaMedia can look for the media you want right away.
+                  </p>
+                  <p>
+                    Just press the lens and type the name of the media you want.
+                  </p>
+              </div>
+          </div>
+          <div class="row cells12">
+              <div class="cell colspan5 cell-explain">
+                  <h3>How much do you have?</h3><hr />
+                 <p>You can see a little info about your movies and series. </p>
+                <p>Just press the statistics section to get a brief resume of all of it.</p>
+              </div>
+              <div class="cell colspan7">
+                  <div class="image-container">
+                      <div class="frame"><img src="https://cloud.githubusercontent.com/assets/4562922/14845442/8464d2ce-0c5e-11e6-9ae2-e0bc2b41009b.png"></div>
+                      <div class="image-overlay">
+                          <h2>Your media</h2>
+                          <p>
+                              The number, the hours and the size.
+                          </p>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
